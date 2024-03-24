163aed889015f32dd740ae1d789c681a5aa9faa1
ardier16 <ardier16@gmail.com>
2024-03-24 16:07:10 +0200

    Add image extension

---
+++ Rarime/Code/Extensions/Image.swift
@@ -0,0 +1,29 @@
+//
+//  Image.swift
+//  Rarime
+//
+//  Created by Maksym Shopynskyi on 24.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    func square(_ size: CGFloat) -> some View {
+        self
+            .resizable()
+            .frame(width: size, height: size)
+    }
+
+    func iconLarge() -> some View {
+        self.square(24)
+    }
+
+    func iconMedium() -> some View {
+        self.square(20)
+    }
+
+    func iconSmall() -> some View {
+        self.square(16)
+    }
+}
