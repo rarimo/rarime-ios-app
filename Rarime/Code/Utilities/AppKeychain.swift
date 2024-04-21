74e66206a90b8b0edd8eee314a8bb48bf0c666b6
ardier16 <ardier16@gmail.com>
2024-04-21 14:40:45 +0300

    Chore code

--- Rarime/Code/Utilities/AppKeychain.swift
+++ Rarime/Code/Utilities/AppKeychain.swift
@@ -8,24 +8,16 @@ enum AppKeychainItemKey: String {
 
 class AppKeychain {
     private static let keychain = Keychain(service: "DistributedLab.Rarime")
-    
-    static func removeAll() throws {
-        try keychain.removeAll()
-    }
-    
+
     static func getValue(_ key: AppKeychainItemKey) throws -> String? {
         try keychain.get(key.rawValue)
     }
-    
+
     static func setValue(_ key: AppKeychainItemKey, _ value: String) throws {
         try keychain.set(value, key: key.rawValue)
     }
-    
+
     static func removeValue(_ key: AppKeychainItemKey) throws {
         try keychain.remove(key.rawValue)
-    }
-    
-    static func removeAllValues() throws {
-        try keychain.removeAll()
     }
 }
