52ead3b98fa09a4c63731c5f6974ce9a9e75028f
ardier16 <ardier16@gmail.com>
2024-04-22 11:23:20 +0300

    Fix codable implementation

--- Rarime/Code/Models/Passport.swift
+++ Rarime/Code/Models/Passport.swift
@@ -14,57 +14,6 @@ struct Passport: Codable {
     var dateOfBirth: String
     var nationality: String
 
-    enum CodingKeys: String, CodingKey {
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case gender
-        case passportImageRaw = "passport_image_raw"
-        case documentType = "document_type"
-        case issuingAuthority = "issuing_authority"
-        case documentNumber = "document_number"
-        case documentExpiryDate = "document_expiry_date"
-        case dateOfBirth = "date_of_birth"
-        case nationality
-    }
-
-    init(
-        firstName: String,
-        lastName: String,
-        gender: String,
-        passportImageRaw: String?,
-        documentType: String,
-        issuingAuthority: String,
-        documentNumber: String,
-        documentExpiryDate: String,
-        dateOfBirth: String,
-        nationality: String
-    ) {
-        self.firstName = firstName
-        self.lastName = lastName
-        self.gender = gender
-        self.passportImageRaw = passportImageRaw
-        self.documentType = documentType
-        self.issuingAuthority = issuingAuthority
-        self.documentNumber = documentNumber
-        self.documentExpiryDate = documentExpiryDate
-        self.dateOfBirth = dateOfBirth
-        self.nationality = nationality
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        firstName = try container.decode(String.self, forKey: .firstName)
-        lastName = try container.decode(String.self, forKey: .lastName)
-        gender = try container.decode(String.self, forKey: .gender)
-        passportImageRaw = try container.decode(String.self, forKey: .passportImageRaw)
-        documentType = try container.decode(String.self, forKey: .documentType)
-        issuingAuthority = try container.decode(String.self, forKey: .issuingAuthority)
-        documentNumber = try container.decode(String.self, forKey: .documentNumber)
-        documentExpiryDate = try container.decode(String.self, forKey: .documentExpiryDate)
-        dateOfBirth = try container.decode(String.self, forKey: .dateOfBirth)
-        nationality = try container.decode(String.self, forKey: .nationality)
-    }
-
     var fullName: String {
         "\(firstName) \(lastName)"
     }
--- Rarime/Code/Models/Transaction.swift
+++ Rarime/Code/Models/Transaction.swift
@@ -12,25 +12,11 @@ struct Transaction: Identifiable, Codable {
     var date: Date
     var type: TransactionType
 
-    enum CodingKeys: String, CodingKey {
-        case id, title, icon, amount, date, type
-    }
-
     init(title: String, icon: String, amount: Double, date: Date, type: TransactionType) {
         self.title = title
         self.icon = icon
         self.amount = amount
         self.date = date
         self.type = type
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
-        title = try container.decode(String.self, forKey: .title)
-        icon = try container.decode(String.self, forKey: .icon)
-        amount = try container.decode(Double.self, forKey: .amount)
-        date = try container.decode(Date.self, forKey: .date)
-        type = try container.decode(TransactionType.self, forKey: .type)
     }
 }
