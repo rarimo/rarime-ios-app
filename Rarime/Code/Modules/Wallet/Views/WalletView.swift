ad2b340576ca939b9c89da536c21453d014af50b
ardier16 <ardier16@gmail.com>
2024-06-05 18:46:11 +0300

    Update wallet views

--- Rarime/Code/Modules/Wallet/Views/WalletView.swift
+++ Rarime/Code/Modules/Wallet/Views/WalletView.swift
@@ -4,23 +4,64 @@ private enum WalletRoute: String, Hashable {
     case receive, send
 }
 
+// TODO: move to model/manager
+enum WalletToken: String {
+    case rmo = "RMO"
+    case usdc = "USDC"
+}
+
+// TODO: move to model/manager
+struct WalletAsset {
+    let token: WalletToken
+    let balance: Double
+    let usdBalance: Double?
+}
+
+// TODO: move to model/manager
+let walletAssets = [
+    WalletAsset(
+        token: WalletToken.rmo,
+        balance: 3,
+        usdBalance: nil
+    ),
+    WalletAsset(
+        token: WalletToken.usdc,
+        balance: 10,
+        usdBalance: 10.2
+    ),
+]
+
 struct WalletView: View {
     @EnvironmentObject private var walletManager: WalletManager
     @EnvironmentObject private var userManager: UserManager
 
     @State private var path: [WalletRoute] = []
-    
+
     @State private var isBalanceFetching = false
-    @State private var cancelables: [Task<(), Never>] = []
+    @State private var cancelables: [Task<Void, Never>] = []
+
+    // TODO: use the token from the manager and save to store
+    @State private var token = WalletToken.rmo
+
+    var selectedAsset: WalletAsset {
+        walletAssets.first { $0.token == token } ?? walletAssets.first!
+    }
 
     var body: some View {
         NavigationStack(path: $path) {
             content.navigationDestination(for: WalletRoute.self) { route in
                 switch route {
                 case .receive:
-                    WalletReceiveView(onBack: { path.removeLast() })
+                    WalletReceiveView(
+                        address: userManager.userAddress,
+                        token: token,
+                        onBack: { path.removeLast() }
+                    )
                 case .send:
-                    WalletSendView(onBack: { path.removeLast() })
+                    WalletSendView(
+                        token: token,
+                        onBack: { path.removeLast() }
+                    )
                 }
             }
         }
@@ -28,20 +69,13 @@ struct WalletView: View {
 
     private var content: some View {
         MainViewLayout {
-            VStack(alignment: .leading, spacing: 12) {
-                header
-                RefreshableScrollView(
-                    onRefresh: {
-                        fetchBalance()
-                        try await Task.sleep(nanoseconds: 1_200_000_000)
-                    }
-                ) { _ in
-                    VStack {
-                        transactionsCard
-                        Spacer()
-                    }
-                    .padding(.horizontal, 12)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    header
+                    AssetsSlider()
+                    transactionsList
                 }
+                .padding(.bottom, 120)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(.backgroundPrimary)
@@ -51,75 +85,90 @@ struct WalletView: View {
     }
 
     private var header: some View {
-        VStack(alignment: .leading, spacing: 32) {
+        VStack(alignment: .leading, spacing: 40) {
             Text("Wallet")
                 .subtitle2()
                 .foregroundStyle(.textPrimary)
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Available RMO")
+            VStack(spacing: 8) {
+                Text("Total Balance")
                     .body3()
                     .foregroundStyle(.textSecondary)
-                if isBalanceFetching {
-                    ProgressView()
-                } else {
-                    Text((userManager.balance / Double(Rarimo.rarimoTokenMantis)).formatted())
-                        .h4()
-                        .foregroundStyle(.textPrimary)
+                HStack(spacing: 8) {
+                    if isBalanceFetching {
+                        ProgressView()
+                    } else {
+                        Text((userManager.balance / Double(Rarimo.rarimoTokenMantis)).formatted())
+                            .h4()
+                            .foregroundStyle(.textPrimary)
+                    }
+                    AppDropdown(value: $token, options: [
+                        DropdownOption(label: WalletToken.rmo.rawValue, value: WalletToken.rmo),
+                        DropdownOption(label: WalletToken.usdc.rawValue, value: WalletToken.usdc),
+                    ])
+                    .padding(.top, 8)
                 }
+                .frame(height: 40)
+                .zIndex(1)
+                Text(try! String(selectedAsset.usdBalance == nil ? "---" : "≈$\(selectedAsset.usdBalance!.formatted())"))
+                    .caption2()
+                    .foregroundStyle(.textSecondary)
             }
-            VStack(spacing: 20) {
-                HorizontalDivider()
-                HStack(spacing: 12) {
-                    AppButton(
-                        variant: .secondary,
-                        text: "Receive",
-                        leftIcon: Icons.arrowDown,
-                        action: { path.append(.receive) }
-                    )
-                    AppButton(
-                        variant: .secondary,
-                        text: "Send",
-                        leftIcon: Icons.arrowUp,
-                        action: { path.append(.send) }
-                    )
-                }
+            .frame(maxWidth: .infinity)
+            .zIndex(1)
+            HStack(spacing: 32) {
+                WalletButton(
+                    text: String(localized: "Receive"),
+                    icon: Icons.arrowDown,
+                    action: { path.append(.receive) }
+                )
+                WalletButton(
+                    text: String(localized: "Send"),
+                    icon: Icons.arrowUp,
+                    action: { path.append(.send) }
+                )
             }
+            .frame(maxWidth: .infinity)
+            HorizontalDivider()
         }
-        .padding(20)
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
         .frame(maxWidth: .infinity)
-        .background(.backgroundPure)
     }
 
-    private var transactionsCard: some View {
-        CardContainer {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Transactions")
-                    .subtitle3()
-                    .foregroundStyle(.textPrimary)
-                ForEach(walletManager.transactions) { tx in
-                    TransactionItem(tx: tx)
-                }
-                if walletManager.transactions.isEmpty {
-                    Text("No transactions yet")
-                        .body3()
-                        .foregroundStyle(.textSecondary)
+    private var transactionsList: some View {
+        VStack {
+            CardContainer {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Transactions")
+                        .subtitle3()
+                        .foregroundStyle(.textPrimary)
+                    ForEach(walletManager.transactions) { tx in
+                        TransactionItem(tx: tx, token: token)
+                    }
+                    if walletManager.transactions.isEmpty {
+                        Text("No transactions yet")
+                            .body3()
+                            .foregroundStyle(.textSecondary)
+                    }
                 }
             }
+            Spacer()
         }
+        .background(.backgroundOpacity)
     }
-    
+
     func fetchBalance() {
         isBalanceFetching = true
-        
+
         let cancelable = Task { @MainActor in
-            
+
             defer {
                 self.isBalanceFetching = false
             }
-            
+
             do {
                 let balance = try await userManager.fetchBalanse()
-                
+
                 self.userManager.balance = Double(balance) ?? 0
             } catch is CancellationError {
                 return
@@ -127,10 +176,10 @@ struct WalletView: View {
                 LoggerUtil.intro.error("failed to fetch balance: \(error.localizedDescription)")
             }
         }
-        
-        self.cancelables.append(cancelable)
+
+        cancelables.append(cancelable)
     }
-    
+
     func cleanup() {
         for cancelable in cancelables {
             cancelable.cancel()
@@ -138,8 +187,30 @@ struct WalletView: View {
     }
 }
 
+private struct WalletButton: View {
+    var text: String
+    var icon: String
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            VStack(spacing: 8) {
+                Image(icon)
+                    .iconMedium()
+                    .foregroundStyle(.textPrimary)
+                    .padding(14)
+                    .background(.componentPrimary, in: Circle())
+                Text(text)
+                    .buttonSmall()
+                    .foregroundStyle(.textSecondary)
+            }
+        }
+    }
+}
+
 private struct TransactionItem: View {
     var tx: Transaction
+    var token: WalletToken
 
     var balanceModifier: String {
         tx.type == .sent ? "-" : "+"
@@ -162,7 +233,7 @@ private struct TransactionItem: View {
                     .foregroundStyle(.textSecondary)
             }
             Spacer()
-            Text("\(balanceModifier)\(tx.amount.formatted()) RMO")
+            Text(try! String("\(balanceModifier)\(tx.amount.formatted()) \(token.rawValue)"))
                 .subtitle5()
                 .foregroundStyle(tx.type == .sent ? .errorMain : .successMain)
         }
