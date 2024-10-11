73e95942baba95753bbf3bcb9da72183599b66d4
ivanlele <ivanlele402@gmail.com>
2024-10-11 15:01:08 +0300

    update the circuits

--- Frameworks/bridge.h
+++ Frameworks/bridge.h
@@ -5,6 +5,8 @@
 #include "witnesscalc_queryIdentity.h"
 #include "witnesscalc_auth.h"
 #include "witnesscalc_registerIdentity_1_256_3_5_576_248_NA.h"
+#include "witnesscalc_registerIdentity_1_256_3_6_576_248_1_2432_5_296.h"
 #include "witnesscalc_registerIdentity_2_256_3_6_336_264_21_2448_6_2008.h"
+#include "witnesscalc_registerIdentity_21_256_3_7_336_264_21_3072_6_2008.h"
 
 #endif /* bridge_h */
