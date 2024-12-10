65d6b867612b0a0d95e6fd7caf454e7f426ca263
ivanlele <ivanlele402@gmail.com>
2024-12-10 15:00:02 +0200

    Add LightRegistrationService for handling light registration verification

---
+++ Rarime/Code/Models/Services/LightRegistrationService.swift
@@ -0,0 +1,111 @@
+import Alamofire
+import Foundation
+
+class LightRegistrationService {
+    let url: URL
+
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func verifySod(_ passport: Passport) async throws -> VerifySodResponseAttributes {
+        var requestURL = url
+        requestURL.append(path: "intergrations/incognito-light-registrator/v1/register")
+
+        let sod = try passport.getSod()
+
+        let signedAttributes = try sod.getSignedAttributes()
+        let encapsulatedContent = try sod.getEncapsulatedContent()
+        let signature = try sod.getSignature()
+
+        let certPem = try passport.getSlaveSodCertificatePem()
+
+        guard let encapsulatedContentDigestAlgorithm = try passport.getEncapsulatedContentDigestAlgorithm(sod) else {
+            throw "invalid encapsulated content digest algorithm"
+        }
+
+        let sodSignatureAlgorithmName = try sod.getSignatureAlgorithm().lowercased()
+
+        var signatureAlgorithm: String
+        if sodSignatureAlgorithmName.contains("rsassapss") {
+            signatureAlgorithm = "RSA-PSS"
+        } else if sodSignatureAlgorithmName.contains("ecdsa") {
+            signatureAlgorithm = "ECDSA"
+        } else if sodSignatureAlgorithmName.contains("rsa") {
+            signatureAlgorithm = "RSA"
+        } else {
+            throw "invalid signature algorithm"
+        }
+
+        let request = VerifySodRequest(
+            data: VerifySodRequestData(
+                id: "",
+                type: "document-sod",
+                attributes: VerifySodRequestAttributes(
+                    hashAlgorithm: encapsulatedContentDigestAlgorithm.rawValue.uppercased(),
+                    signatureAlgorithm: signatureAlgorithm,
+                    signedAttributes: signedAttributes.fullHex,
+                    signature: signature.fullHex,
+                    encapsulatedContent: encapsulatedContent.fullHex,
+                    pemFile: certPem.utf8,
+                    dg15: passport.dg15.fullHex
+                )
+            )
+        )
+
+        let response = try await AF.request(
+            requestURL,
+            method: .post,
+            parameters: request,
+            encoder: JSONParameterEncoder.default
+        )
+        .validate(OpenApiError.catchInstance)
+        .serializingDecodable(VerifySodResponseAttributes.self)
+        .result
+        .get()
+
+        return response
+    }
+}
+
+struct VerifySodRequest: Codable {
+    let data: VerifySodRequestData
+}
+
+struct VerifySodRequestData: Codable {
+    let id, type: String
+    let attributes: VerifySodRequestAttributes
+}
+
+struct VerifySodRequestAttributes: Codable {
+    let hashAlgorithm, signatureAlgorithm, signedAttributes, signature: String
+    let encapsulatedContent, pemFile, dg15: String
+
+    enum CodingKeys: String, CodingKey {
+        case hashAlgorithm = "hash_algorithm"
+        case signatureAlgorithm = "signature_algorithm"
+        case signedAttributes = "signed_attributes"
+        case signature
+        case encapsulatedContent = "encapsulated_content"
+        case pemFile = "pem_file"
+        case dg15
+    }
+}
+
+struct VerifySodResponse: Codable {
+    let data: VerifySodResponseData
+}
+
+struct VerifySodResponseData: Codable {
+    let id, type: String
+    let attributes: VerifySodResponseAttributes
+}
+
+struct VerifySodResponseAttributes: Codable {
+    let signature, documentHash: String
+
+    enum CodingKeys: String, CodingKey {
+        case signature
+        case documentHash = "document_hash"
+    }
+}
