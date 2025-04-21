1834c686f4ca3e581b82a1db95a5aeb2844c13f9
Maksym <ardier16@gmail.com>
2025-04-21 15:13:26 +0300

    Fix blur animation (#162)
    * Fix blur animation
    
    * Add idle blur
    
    * Remove glur

--- Rarime/Code/UI/Views/GlassBottomSheet.swift
+++ Rarime/Code/UI/Views/GlassBottomSheet.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
-struct GlassBottomSheet<Content: View>: View {
+struct GlassBottomSheet<Background: View, Content: View>: View {
+    let background: Background
     let content: Content
 
     var minHeight: CGFloat
     var maxHeight: CGFloat
+    var maxBlur: CGFloat
     
     private let spaceName = "scroll"
    
@@ -15,10 +17,14 @@ struct GlassBottomSheet<Content: View>: View {
     init(
         minHeight: CGFloat,
         maxHeight: CGFloat,
+        maxBlur: CGFloat,
+        @ViewBuilder background: () -> Background,
         @ViewBuilder content: () -> Content
     ) {
         self.minHeight = minHeight
         self.maxHeight = maxHeight
+        self.maxBlur = maxBlur
+        self.background = background()
         self.content = content()
         _currentHeight = State(initialValue: minHeight)
     }
@@ -28,39 +34,43 @@ struct GlassBottomSheet<Content: View>: View {
         return min(max(proposedHeight, minHeight), maxHeight)
     }
     
+    var blurRadius: CGFloat {
+        return (effectiveHeight - minHeight) / maxHeight * maxBlur
+    }
+    
     var body: some View {
         GeometryReader { proxy in
             ZStack(alignment: .top) {
-                TransparentBlurView(removeAllFilters: false)
-                VStack(spacing: 24) {
-                    Capsule()
-                        .fill(.bgComponentBaseHovered)
-                        .frame(width: 36, height: 5)
-                    ScrollView(.vertical, showsIndicators: false) {
+                background
+                    .blur(radius: blurRadius)
+                ZStack(alignment: .top) {
+                    VStack(spacing: 24) {
+                        Capsule()
+                            .fill(.bgComponentBaseHovered)
+                            .frame(width: 36, height: 5)
                         ChildOffsetReader(offset: $scrollOffset) {
                             content
                         }
                     }
-                    .coordinateSpace(name: spaceName)
-                    .scrollDisabled(currentHeight < maxHeight)
                 }
+                .frame(width: proxy.size.width, height: effectiveHeight, alignment: .top)
+                .offset(y: proxy.size.height - effectiveHeight)
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, state, _ in
+                            if currentHeight < maxHeight || (currentHeight == maxHeight && scrollOffset <= 0) {
+                                state = value.translation.height
+                            }
+                        }
+                        .onEnded { value in
+                            let proposedHeight = currentHeight - value.translation.height
+                            let midpoint = (minHeight + maxHeight) / 2
+                            currentHeight = proposedHeight > midpoint ? maxHeight : minHeight
+                        }
+                )
+                .animation(.spring(response: 0.5, dampingFraction: 0.8), value: dragOffset)
             }
-            .frame(width: proxy.size.width, height: effectiveHeight, alignment: .top)
-            .offset(y: proxy.size.height - effectiveHeight)
-            .gesture(
-                DragGesture()
-                    .updating($dragOffset) { value, state, _ in
-                        if currentHeight < maxHeight || (currentHeight == maxHeight && scrollOffset <= 0) {
-                           state = value.translation.height
-                       }
-                    }
-                    .onEnded { value in
-                        let proposedHeight = currentHeight - value.translation.height
-                        let midpoint = (minHeight + maxHeight) / 2
-                        currentHeight = proposedHeight > midpoint ? maxHeight : minHeight
-                    }
-            )
-            .animation(.spring(response: 0.5, dampingFraction: 0.8), value: dragOffset)
+            .frame(width: proxy.size.width, height: proxy.size.height, alignment: .top)
         }
         .ignoresSafeArea(edges: .bottom)
     }
@@ -68,9 +78,18 @@ struct GlassBottomSheet<Content: View>: View {
 
 #Preview {
     VStack {
-        GlassBottomSheet(minHeight: 360, maxHeight: 720) {
+        GlassBottomSheet(
+            minHeight: 360,
+            maxHeight: 720,
+            maxBlur: 20,
+            background: {
+                Image(Images.dotCountry)
+                    .resizable()
+                    .scaledToFit()
+            }
+        ) {
             VStack(spacing: 8) {
-                ForEach(1...10, id: \.self) { _ in
+                ForEach(1 ... 10, id: \.self) { _ in
                     RoundedRectangle(cornerRadius: 16)
                         .fill(.successMain)
                         .frame(width: .infinity, height: 250)
