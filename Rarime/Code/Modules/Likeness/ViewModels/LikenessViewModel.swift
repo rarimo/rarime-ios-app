189f3fcfd8634677a48af1cec2ccc4f3c9062573
Ivan Lele <110336129+ivanlele@users.noreply.github.com>
2025-04-22 17:56:11 +0700

    DRAFT: Face scanning (#164)
    * Add Likeness module with FaceCaptureSession, ViewModel, and View; include DebugAssets and PreviewUtils
    
    * Refactor Likeness module: update LikenessView and LikenessViewModel for improved image handling and UI; add FaceScan assets and localizable strings
    
    * Refactor LikenessViewModel and LikenessView: change cameraManager to var, add pauseScanning method, and update button styles for consistency
    
    * Add haptic feedback to picture taking and retaking actions in LikenessView
    
    * Update haptic feedback intensity for picture taking, retaking, and confirmation actions in LikenessView
    
    * Rename LikenessView to FaceLikenessView and add LikenessProcessingView for improved image processing and UI flow
    
    * Replace debugFace.jpg with c0546ed093502cca87c663a30a67adf2.jpeg in debugFace imageset for improved asset management
    
    * Update preview in LikenessProcessingView to comment out unused task and clarify usage

---
+++ Rarime/Code/Modules/Likeness/ViewModels/LikenessViewModel.swift
@@ -0,0 +1,101 @@
+import Web3
+
+import SwiftUI
+
+protocol LikenessProcessingTask: CaseIterable {
+    var rawValue: Int { get }
+    var description: String { get }
+    var progressTime: Int { get }
+}
+
+enum LikenessProcessingRecoveryTask: Int, LikenessProcessingTask {
+    case downloadingCircuitData = 0
+    case extractionImageFeatures = 1
+    case runningZKMK = 2
+    case overridingAccess = 3
+    
+    var description: String {
+        switch self {
+        case .downloadingCircuitData: return "Downloading circuit data"
+        case .extractionImageFeatures: return "Extracting image features"
+        case .runningZKMK: return "Running ZKML"
+        case .overridingAccess: return "Overriding access"
+        }
+    }
+    
+    var progressTime: Int {
+        switch self {
+        case .downloadingCircuitData: return 15
+        case .extractionImageFeatures: return 5
+        case .runningZKMK: return 10
+        case .overridingAccess: return 7
+        }
+    }
+}
+
+enum LikenessProcessingRegisterTask: Int, CaseIterable, LikenessProcessingTask {
+    case downloadingCircuitData = 0
+    case extractionImageFeatures = 1
+    case runningZKMK = 2
+    case creatingAccount = 3
+    
+    var description: String {
+        switch self {
+        case .downloadingCircuitData: return "Downloading circuit data"
+        case .extractionImageFeatures: return "Extracting image features"
+        case .runningZKMK: return "Running ZKML"
+        case .creatingAccount: return "Registering recovery method"
+        }
+    }
+    
+    var progressTime: Int {
+        switch self {
+        case .downloadingCircuitData: return 15
+        case .extractionImageFeatures: return 5
+        case .runningZKMK: return 10
+        case .creatingAccount: return 7
+        }
+    }
+}
+
+class LikenessViewModel: ObservableObject {
+    @Published var currentFrame: CGImage?
+        
+    private var cameraManager = FaceCaptureSession()
+        
+    @Published var cameraTask: Task<Void, Never>? = nil
+        
+    func startScanning() {
+        cameraTask = Task {
+            await cameraManager.startSession()
+                
+            await handleCameraPreviews()
+        }
+    }
+        
+    func stopScanning() {
+        cameraManager.stopSession()
+            
+        cameraTask?.cancel()
+    }
+    
+    func pauseScanning() {
+        cameraManager.stopSession()
+    }
+        
+    func handleCameraPreviews() async {
+        for await image in cameraManager.previewStream {
+            Task { @MainActor in
+                currentFrame = image
+                    
+                handleFaceImage(image)
+            }
+        }
+    }
+        
+    func handleFaceImage(_ image: CGImage) {}
+    
+    func clearImages() {
+        currentFrame = nil
+    }
+}
