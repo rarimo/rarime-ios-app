1a12d177b9d63ae84e391ba5829f1a41eca4ff6f
Maksym <ardier16@gmail.com>
2025-06-06 18:26:26 +0300

    Fix app icon manager (#208)

--- Rarime/Code/Managers/AppIconManager.swift
+++ Rarime/Code/Managers/AppIconManager.swift
@@ -18,7 +18,7 @@ class AppIconManager: ObservableObject {
         let oldIcon = self.appIcon
         self.appIcon = icon
 
-        UIApplication.shared.setAlternateIconName(icon == .black ? nil : icon.image) { error in
+        UIApplication.shared.setAlternateIconName(icon.isDefault ? nil : icon.rawValue) { error in
             if let error = error {
                 LoggerUtil.common.error("Error setting the app icon: \(error.localizedDescription, privacy: .public)")
                 self.appIcon = oldIcon
